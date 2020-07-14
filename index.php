b275b9682eb02b04e9c2def51819537317dd7ac0
Sachin Kumar <56272723+hackcoderr@users.noreply.github.com>
2020-07-14 20:57:11 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<pre>
+<?php
+
+print `ifconfig`;
+
+
+
+
+?>
+
+img src​="<?php echo getenv('url') ?>/1.jpg">
+img src​="<?php echo getenv('url') ?>/2.jpg">
+img src​="<?php echo getenv('url') ?>/3.jpg">
+
+</pre>
